d38373ecda717440dae4b3e26933a4b881715b3c
root <raphael.mistretta9@gmail.com>
2024-02-23 09:54:17 +0000

    change

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Prints numbers from 0 to 9.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("_putchar\n");
+
+	return (0);
+}
