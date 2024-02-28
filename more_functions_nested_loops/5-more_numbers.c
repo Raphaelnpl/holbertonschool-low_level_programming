28c0b2118fd6108d7a3f453cfb442cb5a736b8c3
root <raphael.mistretta9@gmail.com>
2024-02-28 13:52:03 +0000

    change

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * void more_numbers - check the code
+ *
+ * Return: Always 0.
+ */
+void more_numbers
+{
+	int x, y;
+
+	for (x = 0; x <= 9; x++)
+	{
+		for (y = 0; y<= , y++)
+		{
+			if (y >= 10)
+			{
+				_putchar(y / 10 + '0');
+			}
+			_putchar(y % 10 + '0');
+		}
+		_putchar('\n');
+	}
+}	
