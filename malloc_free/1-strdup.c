52bd7535fc4f7c39ac7fd145e79168020280db30
Raphael Melnique <158978700+Raphaelnpl@users.noreply.github.com>
2024-03-12 23:29:22 +0000

    change

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _strdup - Duplicate a string
+ * @str: String to duplicate
+ *
+ * Return: Pointer to the duplicated string
+ */
+char *_strdup(char *str)
+{
+	int i;
+	int j;
+	int size_str;
+	char *s;
+
+	if (str == 0)
+	{
+		return (0);
+	}
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+	}
+	size_str = i;
+	s = malloc(size_str * sizeof(char) + 1);
+
+	if (s == 0)
+	{
+		return (NULL);
+	}
+	
+	for (j = 0; j < i; j++)
+	{
+		s[j] = str[j];
+	}
+	return (s);
+}
