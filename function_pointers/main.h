d76c5fbce5d3e16ae436cdaffdf97ecda638859c
root <raphael.mistretta9@gmail.com>
2024-03-21 16:10:02 +0000

    change

--- function_pointers/main.h
+++ function_pointers/main.h
@@ -1,11 +1,18 @@
-int (*get_op_func(char *s))(int, int);
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stddef.h> /* pour size_t */
+
+/* void array_iterator(int *array, size_t size, void (*action)(int)); */
+
+#endif /* MAIN_H */
+
 int op_add(int a, int b);
 int op_sub(int a, int b);
 int op_mul(int a, int b);
 int op_div(int a, int b);
 int op_mod(int a, int b);
 int int_index(int *array, int size, int (*cmp)(int));
-void array_iterator(int *array, size_t size, void (*action)(int));
 void print_name_as_is(char *name);
 int _putchar(char c);
 void print_name_uppercase(char *name);
