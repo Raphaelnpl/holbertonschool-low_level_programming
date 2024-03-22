a19ccbd0de9fa353cdcb3f66b13657e9876f39f4
root <raphael.mistretta9@gmail.com>
2024-03-22 09:35:29 +0000

    Write a function that returns the sum of all its parameters

---
+++ variadic_functions/variadic_functions.h
@@ -0,0 +1,8 @@
+#ifndef variadic_functions_h
+#define variadic_functions_h
+
+#include <stddef.h> /* pour size_t */
+
+int sum_them_all(const unsigned int n, ...);
+
+#endif /* variadic_functions.h */
