43910e5e1e961696a4c4e3465ff58f4ba9f71e9f
Raphaelnpl <raphael.mistretta9@gmail.com>
2024-06-18 09:25:29 +0000

    new puts message

---
+++ hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+int puts ( const char * string );
+
+{
+	puts "Programming is like building a multilingual puzzle"
+
+	return (0);
+}
