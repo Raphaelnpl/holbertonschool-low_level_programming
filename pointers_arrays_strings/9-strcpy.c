f9a3a11ea474c1ca0dbf8a17691192cb238c6395
Raphaelnpl <raphael.mistretta9@gmail.com>
2024-06-30 14:16:10 +0000

    change

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _strcpy - Copies the string pointed to by src to the buffer pointed 
+ * _strcpy - Copies the string pointed to by src to the buffer pointed
  * @dest: Pointer to the destination buffer.
  * @src: Pointer to the source string.
  *
