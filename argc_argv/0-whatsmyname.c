ae4badac888427d37a72c7ade030882f8388b79c
Raphaelnpl <raphael.mistretta9@gmail.com>
2024-07-05 17:52:48 +0000

    new change

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ * @argc: Number of command line arguments
+ * @argv: Array containing the command line arguments
+ *
+ * Return: Always 0
+ */
+int main(int argc __attribute__((unused)), char *argv[])
+{
+    printf("%s\n", argv[0]);
+    return (0);
+}
