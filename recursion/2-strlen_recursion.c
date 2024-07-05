6f721ddec9c6f001532aab6318c6ee1c4246cb56
Raphaelnpl <raphael.mistretta9@gmail.com>
2024-07-05 10:11:20 +0000

    function that returns the length of a string

---
+++ recursion/2-strlen_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - check the code
+ *
+ * @s : r
+ * @return: s
+ */
+int _strlen_recursion(char *s)
+
+{
+	if (*s == '\0')
+	{
+		return (0);
+	}
+	else
+	{
+		return (1 + _strlen_recursion(s + 1));
+	}
+}
