50fbe7a0565fbcbe20a3d0c78b130f87bf875e8d
Raphaelnpl <raphael.mistretta9@gmail.com>
2024-07-08 20:28:03 +0000

    change

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -1,8 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "main.h"
-#include <stdio.h>
-#include <stdlib.h>
 
 /**
  * _strdup - Duplicate a string
@@ -12,30 +10,31 @@
  */
 char *_strdup(char *str)
 {
-	int i;
-	int j;
-	int size_str;
-	char *s;
+        int i;
+        int j;
+        int size_str;
+        char *s;
 
-	if (str == 0)
-	{
-		return (0);
-	}
+        if (str == 0)
+        {
+                return (0);
+        }
 
-	for (i = 0; str[i] != '\0'; i++)
-	{
-	}
-	size_str = i;
-	s = malloc(size_str * sizeof(char) + 1);
+        for (i = 0; str[i] != '\0'; i++)
+        {
+        }
+        size_str = i;
+        s = malloc(size_str * sizeof(char) + 1);
 
-	if (s == 0)
-	{
-		return (NULL);
-	}
-	
-	for (j = 0; j < i; j++)
-	{
-		s[j] = str[j];
-	}
-	return (s);
+        if (s == 0)
+        {
+                return (NULL);
+        }
+
+        for (j = 0; j < i; j++)
+        {
+                s[j] = str[j];
+        }
+        s[j] = '\0';
+        return (s);
 }
