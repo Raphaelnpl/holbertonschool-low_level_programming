041f94155ddae2aa8a13fd58293ef2b5fc350bf5
Raphael Melnique <158978700+Raphaelnpl@users.noreply.github.com>
2024-07-15 12:10:34 +0200

    Delete function_pointers.h

--- function_pointers.h
+++
@@ -1,8 +0,0 @@
-#ifndef FUNCTION_POINTERS_H
-#define FUNCTION_POINTERS_H
-
-#include <stddef.h> /* pour size_t */
-
-int int_index(int *array, int size, int (*cmp)(int));
-
-#endif /* FUNCTION_POINTERS_H */
