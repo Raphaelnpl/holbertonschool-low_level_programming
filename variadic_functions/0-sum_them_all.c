0490e11e255faf7c26b8377e21e353da458d0d04
Raphael Melnique <158978700+Raphaelnpl@users.noreply.github.com>
2024-07-18 11:49:35 +0000

    change

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -14,7 +14,7 @@ int sum_them_all(const unsigned int n, ...)
 	unsigned int i;
 
 	if (n == 0)
-		return 0;
+		return (0);
 
 	va_start(args, n);
 
@@ -25,5 +25,5 @@ int sum_them_all(const unsigned int n, ...)
 
 	va_end(args);
 
-	return sum;
+	return (sum);
 }
