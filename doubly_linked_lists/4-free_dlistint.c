961fa53645942950eaaca47580d81284f335caaf
Raphael Melnique <158978700+Raphaelnpl@users.noreply.github.com>
2024-08-02 19:26:58 +0000

    change

--- doubly_linked_lists/4-free_dlistint.c
+++ doubly_linked_lists/4-free_dlistint.c
@@ -1,6 +1,13 @@
 #include "lists.h"
-#include <stdlib.h>  /* Include this line */
+#include <stdlib.h>
 
+/**
+ * free_dlistint - Frees a doubly linked list
+ * @head: Pointer to the head of the doubly linked list
+ *
+ * Description: This function takes a pointer to the head of a doubly
+ * linked list and frees all the nodes in the list.
+ */
 void free_dlistint(dlistint_t *head)
 {
 	dlistint_t *temp;
