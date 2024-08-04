69d157bad6e3488ec70f37091a9039288e323760
Raphael Melnique <158978700+Raphaelnpl@users.noreply.github.com>
2024-08-04 15:21:27 +0000

    chanhe

--- file_io/main.h
+++ file_io/main.h
@@ -2,7 +2,11 @@
 #define MAIN_H
 
 #include <stddef.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
 
-int create_file(const char *filename, char *text_content);
+int append_text_to_file(const char *filename, char *text_content);
 
 #endif /* MAIN_H */
