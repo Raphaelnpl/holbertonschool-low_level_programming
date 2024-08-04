8ff9854042e7caef7ec3923836e99c6fe493b9f9
Raphael Melnique <158978700+Raphaelnpl@users.noreply.github.com>
2024-08-04 15:50:37 +0000

    chage

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -1,84 +1,79 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <fcntl.h>
-#include <unistd.h>
-#include <errno.h>
-#include <string.h>
 #include "main.h"
 
-#define BUFFER_SIZE 1024
-
 /**
- * error_exit - Print an error message and exit the program.
- * @code: Exit code.
- * @message: Error message format.
- * @file: File name (used in error messages).
+ * _errexit - print error message and exit
+ * @str: err message as string
+ * @file: file name as string
+ * @code: exit code
+ * Return: void
  */
-void error_exit(int code, const char *message, const char *file)
+void _errexit(char *str, char *file, int code)
 {
-	dprintf(STDERR_FILENO, message, file);
+	dprintf(STDERR_FILENO, str, file);
 	exit(code);
 }
 
 /**
- * main - Copies the content of a file to another file.
- * @argc: Number of arguments.
- * @argv: Argument vector.
+ * _cp - copy source file to destination file
+ * @file_from: source file
+ * @file_to: destination file
  *
- * Return: 0 on success, or one of the exit codes specified in the task.
+ * Return: void
+ */
+void _cp(char *file_from, char *file_to)
+{
+	int fd1, fd2, numread, numwrote;
+	char buffer[1024];
+
+	fd1 = open(file_from, O_RDONLY);
+	if (fd1 == -1)
+		_errexit("Error: Can't read from file %s\n", file_from, 98);
+
+	fd2 = open(file_to, O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	if (fd2 == -1)
+		_errexit("Error: Can't write to %s\n", file_to, 99);
+
+	numread = 1024;
+	while (numread == 1024)
+	{
+		numread = read(fd1, buffer, 1024);
+		if (numread == -1)
+			_errexit("Error: Can't read from file %s\n", file_from, 98);
+
+		numwrote = write(fd2, buffer, numread);
+
+		if (numwrote == -1)
+			_errexit("Error: Can't write to %s\n", file_to, 99);
+	}
+
+	if (numread == -1)
+		_errexit("Error: Can't read from file %s\n", file_from, 98);
+	if (close(fd2) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd2);
+		exit(100);
+	}
+	if (close(fd1) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd2);
+		exit(100);
+	}
+}
+/**
+ *main - copies a file to another file
+ *@argc: number of arguments passed to function
+ *@argv: array containing arguments
+ *Return: 0 on success
  */
 int main(int argc, char *argv[])
 {
-	int fd_from, fd_to;
-	ssize_t bytes_read, bytes_written;
-	char buffer[BUFFER_SIZE];
-	mode_t perm = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH;
-
 	if (argc != 3)
 	{
-		error_exit(97, "Usage: cp file_from file_to\n", NULL);
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
 	}
 
-	fd_from = open(argv[1], O_RDONLY);
-	if (fd_from == -1)
-	{
-		error_exit(98, "Error: Can't read from file %s\n", argv[1]);
-	}
-
-	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, perm);
-	if (fd_to == -1)
-	{
-		close(fd_from);
-		error_exit(99, "Error: Can't write to %s\n", argv[2]);
-	}
-
-	while ((bytes_read = read(fd_from, buffer, BUFFER_SIZE)) > 0)
-	{
-		bytes_written = write(fd_to, buffer, bytes_read);
-		if (bytes_written != bytes_read)
-		{
-			close(fd_from);
-			close(fd_to);
-			error_exit(99, "Error: Can't write to %s\n", argv[2]);
-		}
-	}
-
-	if (bytes_read == -1)
-	{
-		close(fd_from);
-		close(fd_to);
-		error_exit(98, "Error: Can't read from file %s\n", argv[1]);
-	}
-
-	if (close(fd_from) == -1)
-	{
-		error_exit(100, "Error: Can't close fd %d\n", fd_from);
-	}
-
-	if (close(fd_to) == -1)
-	{
-		error_exit(100, "Error: Can't close fd %d\n", fd_to);
-	}
+	_cp(argv[1], argv[2]);
 
 	return (0);
 }
--- file_io/main.h
+++ file_io/main.h
@@ -1,13 +1,21 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+/* POSIX standards */
+#include <unistd.h>
+
+/* OPEN, READ, WRITE, CLOSE */
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
-#include <unistd.h>
 
-/* Function prototypes */
-void error_exit(int code, const char *message, const char *file);
+int _putchar(char c);
+ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
+int append_text_to_file(const char *filename, char *text_content);
 
-#endif /* MAIN_H */
+
+#endif /* MAIN.H */
